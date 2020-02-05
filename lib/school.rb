c650c3e87966a19d91b1109319fe053315d70142
Stacy Johns <stacykjohns@icloud.com>
2020-02-05 21:07:06 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+  def initialize(roster)
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    roster[grade] ||= []
+    roster[grade] << name
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    sorted = {}
+    roster.each do |grade, names|
+      sorted[grade] = names.sort
+    end
+    sorted
+  end
+end
